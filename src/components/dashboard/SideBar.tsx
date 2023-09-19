117dda100fa5e9b0a29c9d72cc4dd3a3856e69e5
Qulbe12 <qulbehussain@outlook.com>
2023-09-19 21:25:59 +0500

    feat: created new component named side bar

---
+++ src/components/dashboard/SideBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from "native-base";
+
+const SideBar = () => {
+    return (
+        <View>
+        <Text>Hi from side bar</Text>
+        </View>
+    );
+};
+
+export default SideBar;
