b05e1816db1267e14099ac2905ac263e22a7de4b
xImam00 <imambutt382#gmail>
2023-09-26 10:46:03 -0700

    feat:it is the updated component

--- src/components/screens/ContactList.tsx
+++ src/components/screens/ContactList.tsx
@@ -1,85 +1,88 @@
 import React from 'react';
-import { Box, Center, Input, Text, VStack, Icon, IconButton, HStack, } from "native-base";
+import { Box, Center, Input, Text, VStack, Icon, IconButton, HStack, Flex, ScrollView, } from "native-base";
 
 const ContactList = () => {
     return (
-        <VStack space={4} py={7} flex={1}>
-            <Center>
-                <Box>
-                    <Input placeholder="Search" w="60%" borderRadius="10" py="1" px="2" borderColor="orange.600"></Input>
-                </Box>
-            </Center>
-            <VStack py={10}  >
-                <HStack space={2}    >
-                    <Box bg="pink.200" h="28%" borderRadius="50" w="15%" justifyContent="center"    >
-                        <Center>
-                            <Text>AK</Text>
-                        </Center>
+        <ScrollView        >
+            <VStack py={10} flex={1}>
+                <Center>
+                    <Box>
+                        <Input placeholder="Search" w="60%" borderRadius="10" px="2" borderColor="orange.600"      ></Input>
                     </Box>
-                    <VStack>
-                        <Text>Alex Kade</Text>
-                        <Text>Available</Text>
-                    </VStack>
-                </HStack>
-                <HStack>
-                    <Box bg="pink.200" h="28%" borderRadius="50" w="15%" justifyContent="center">
-                        <Center>
-                            <Text>AK</Text>
-                        </Center>
-                    </Box>
-                    <VStack>
-                        <Text>Alex Kade</Text>
-                        <Text>Available</Text>
-                    </VStack>
-                </HStack>
-                <HStack>
-                    <Box bg="pink.200" h="28%" borderRadius="50" w="15%" justifyContent="center">
-                        <Center>
-                            <Text>AK</Text>
-                        </Center>
-                    </Box>
-                    <VStack>
-                        <Text>Alex Kade</Text>
-                        <Text>Available</Text>
-                    </VStack>
-                </HStack>
-                <HStack>
-                    <Box bg="pink.200" h="28%" borderRadius="50" w="15%" justifyContent="center">
-                        <Center>
-                            <Text>AK</Text>
-                        </Center>
-                    </Box>
-                    <VStack>
-                        <Text>Alex Kade</Text>
-                        <Text>Available</Text>
-                    </VStack>
-                </HStack>
-                <HStack>
-                    <Box bg="pink.200" h="35%" borderRadius="50" w="15%" justifyContent="center">
-                        <Center>
-                            <Text>AK</Text>
-                        </Center>
-                    </Box>
-                    <VStack>
-                        <Text>Alex Kade</Text>
-                        <Text>Available</Text>
-                    </VStack>
-                </HStack>
-                <HStack>
-                    <Box bg="pink.200" h="28%" borderRadius="50" w="15%" justifyContent="center">
-                        <Center>
-                            <Text>AK</Text>
-                        </Center>
-                    </Box>
-                    <VStack>
-                        <Text>Alex Kade</Text>
-                        <Text>Available</Text>
-                    </VStack>
-                </HStack>
+                </Center>
+                <VStack py={10} flex={1} space={4}>
+                    <HStack flex={1} space={5}   >
+                        <Box bg="orange.600" h="90%" borderRadius="50" w="17%" justifyContent="center" mt={1} left={2}  >
+                            <Center>
+                                <Text fontSize="xl" fontWeight="bold" color="white">AK</Text>
+                            </Center>
+                        </Box>
+                        <VStack h="47" w="20%" justifyContent="center" my={3}>
+                            <Text fontSize="xl"     >Alex Kade</Text>
+                            <Text fontSize="md" fontWeight="hairline" color="gray.900">03*********</Text>
+                        </VStack>
+                    </HStack>
+                    <HStack flex={1} space={5}   >
+                        <Box bg="orange.600" h="90%" borderRadius="50" w="17%" justifyContent="center" mt={1} left={2} >
+                            <Center>
+                                <Text fontSize="xl" fontWeight="bold" color="white">AK</Text>
+                            </Center>
+                        </Box>
+                        <VStack h="47" w="20%" justifyContent="center" my={3}    >
+                            <Text fontSize="xl"      >Alex Kade</Text>
+                            <Text fontWeight="hairline" fontSize="md" color="gray.900">03*********</Text>
+                        </VStack>
+                    </HStack>
+                    <HStack flex={1} space={5}  >
+                        <Box bg="orange.600" h="90%" borderRadius="50" w="17%" justifyContent="center" mt={1} left={2}  >
+                            <Center>
+                                <Text fontSize="xl" fontWeight="bold" color="white">AK</Text>
+                            </Center>
+                        </Box>
+                        <VStack h="47" w="20%" justifyContent="center" my={3}   >
+                            <Text fontSize="xl"     >Alex Kade</Text>
+                            <Text fontWeight="hairline" fontSize="md" color="gray.900">03*********</Text>
+                        </VStack>
+                    </HStack>
+                    <HStack flex={1} space={5}  >
+                        <Box bg="orange.600" h="90%" borderRadius="50" w="17%" justifyContent="center" mt={1} left={2}  >
+                            <Center>
+                                <Text fontSize="xl" fontWeight="bold" color="white">AK</Text>
+                            </Center>
+                        </Box>
+                        <VStack h="47" w="20%" justifyContent="center" my={3}>
+                            <Text fontSize="xl"     >Alex Kade</Text>
+                            <Text fontWeight="hairline" fontSize="md" color="gray.900"       >03*********</Text>
+                        </VStack>
+                    </HStack>
+                    <HStack flex={1} space={5}  >
+                        <Box bg="orange.600" h="90%" borderRadius="50" w="17%" justifyContent="center" mt={1} left={2} >
+                            <Center>
+                                <Text fontSize="xl" fontWeight="bold" color="white">AK</Text>
+                            </Center>
+                        </Box>
+                        <VStack h="47" w="20%" justifyContent="center" my={3}  >
+                            <Text fontSize="xl"      >Alex Kade</Text>
+                            <Text fontWeight="hairline" fontSize="md" color="gray.900">03*********</Text>
+                        </VStack>
+                    </HStack>
+                    <HStack flex={1} space={5}  >
+                        <Box bg="orange.600" h="90%" borderRadius="50" w="17%" justifyContent="center" mt={1} left={2} >
+                            <Center>
+                                <Text fontSize="xl" fontWeight="bold" color="white">AK</Text>
+                            </Center>
+                        </Box>
+                        <VStack h="47" w="20%" justifyContent="center" my={3}    >
+                            <Text fontSize="xl"    >Alex Kade</Text>
+                            <Text fontWeight="hairline" fontSize="md" color="gray.900">03*********</Text>
+                        </VStack>
+                    </HStack>
+
+                </VStack>
 
             </VStack>
+        </ScrollView>
 
-        </VStack>
     );
 };
 
